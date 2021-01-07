8dc382919561d4e670550cc76eb486c2bfe2f115
unknown <rbbelem@gmail.com>
2021-01-06 23:14:48 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/ProjetoSemanaSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemanaSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemanaSuperiorApplication.class, args);
+	}
+
+}
